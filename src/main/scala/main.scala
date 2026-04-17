4be931acb9d84e4a8a76c494b86ed29277483e72
faikidine <98451900+faikidine@users.noreply.github.com>
2026-04-17 14:35:18 +0200

    init

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,73 +1,84 @@
-//#imports
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior, ActorSystem}
 import akka.actor.typed.scaladsl.Behaviors
-//#imports
+import scala.collection.immutable.TreeMap
 
-//#hello-world-actor
-object HelloWorld {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String, from: ActorRef[Greet])
+// Marker trait for Kryo serialization
+//trait SerializableMessage
 
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    context.log.info("Hello {}!", message.whom)
-    message.replyTo ! Greeted(message.whom, context.self)
+// Actions ADT
+sealed trait Action[T]
+case class FilterAction[T](f: T => Boolean) extends Action[T]
+case class MapAction[T](f: T => T) extends Action[T]
+
+// Messages
+case class WorkBatch[T](index: Int, data: Vector[T], actions: List[Action[T]], replyTo: ActorRef[WorkerResponse[T]])
+sealed trait WorkerResponse[T]
+case class WorkDone[T](index: Int, result: Vector[T], worker: ActorRef[WorkBatch[T]]) extends WorkerResponse[T]
+
+object Worker {
+  def apply[T](): Behavior[WorkBatch[T]] = Behaviors.receiveMessage { msg =>
+    // Apply the sequence of operations to the batch
+    val processed = msg.actions.foldLeft(msg.data) { (acc, action) =>
+      action match {
+        case FilterAction(f) => acc.filter(f)
+        case MapAction(f)    => acc.map(f)
+      }
+    }
+    msg.replyTo ! WorkDone(msg.index, processed, msg.replyTo.unsafeUpcast)
     Behaviors.same
   }
 }
-//#hello-world-actor
 
-//#hello-world-bot
-object HelloWorldBot {
-
-  def apply(max: Int): Behavior[HelloWorld.Greeted] = {
-    bot(0, max)
-  }
-
-  private def bot(greetingCounter: Int, max: Int): Behavior[HelloWorld.Greeted] =
-    Behaviors.receive { (context, message) =>
-      val n = greetingCounter + 1
-      context.log.info("Greeting {} for {}", n, message.whom)
-      if (n == max) {
-        Behaviors.stopped
-      } else {
-        message.from ! HelloWorld.Greet(message.whom, context.self)
-        bot(n, max)
-      }
-    }
-}
-//#hello-world-bot
-
-//#hello-world-main
-object HelloWorldMain {
-
-  final case class SayHello(name: String)
-
-  def apply(): Behavior[SayHello] =
+object Master {
+  def apply[T](data: Vector[T], actions: List[Action[T]]): Behavior[WorkerResponse[T]] =
     Behaviors.setup { context =>
-      val greeter = context.spawn(HelloWorld(), "greeter")
+      // Now we have
+      // [list 1024, list 1024, ..., list 1024]
+      val partitions = data.grouped(1024).toVector
+      val partitionCount = partitions.size
 
-      Behaviors.receiveMessage { message =>
-        val replyTo = context.spawn(HelloWorldBot(max = 3), message.name)
-        greeter ! HelloWorld.Greet(message.name, replyTo)
-        Behaviors.same
+      // Spawn 8 workers
+      val workers = (1 to 8).map(i => context.spawn(Worker[T](), s"worker-$i"))
+
+      def active(nextIdx: Int, results: TreeMap[Int, Vector[T]]): Behavior[WorkerResponse[T]] =
+        Behaviors.receiveMessage {
+          case WorkDone(idx, res, worker) =>
+            val newResults = results + (idx -> res)
+
+            if (newResults.size == partitionCount) {
+              // All work done: Reconstitute in order
+              val finalData = newResults.values.flatten
+              println(s"--- Processing Complete ---")
+              finalData.foreach(println)
+              Behaviors.stopped
+            } else {
+              // Assign next available partition to the worker that just finished
+              if (nextIdx < partitionCount) {
+                worker ! WorkBatch(nextIdx, partitions(nextIdx), actions, context.self)
+                active(nextIdx + 1, newResults)
+              } else {
+                active(nextIdx, newResults)
+              }
+            }
+        }
+
+      // Initial assignment: give each worker one partition to start
+      val firstBatchSize = math.min(8, partitionCount)
+      for (i <- 0 until firstBatchSize) {
+        workers(i) ! WorkBatch(i, partitions(i), actions, context.self)
       }
+
+      active(firstBatchSize, TreeMap.empty)
     }
-
-  //#hello-world-main
-  def main(args: Array[String]): Unit = {
-
-    val system: ActorSystem[HelloWorldMain.SayHello] =
-      ActorSystem(HelloWorldMain(), "hello")
-
-    system ! HelloWorldMain.SayHello("World")
-    system ! HelloWorldMain.SayHello("Akka")
-
-
-    Thread.sleep(3000)
-    system.terminate()
-  }
-  //#hello-world-main
 }
-//#hello-world-main
+
+@main
+def main(): Unit = {
+  val rawData = (1 to 5000).toVector // Example data
+  val actions = List(
+    FilterAction[Int](_ < 100),    // Logic: Keep numbers < 100
+    MapAction[Int](_ * 10)         // Logic: Multiply by 10
+  )
+
+  val system = ActorSystem(Master(rawData, actions), "ProcessingSystem")
+}
